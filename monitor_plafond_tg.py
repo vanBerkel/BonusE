30f312e7379c5e9d01a645d4f203cb18c5933400
vanBerkel <annispmt92@hotmail.it>
2025-12-17 00:57:07 +0100

    Create monitor_plafond_tg.py

---
+++ monitor_plafond_tg.py
@@ -0,0 +1,60 @@
+import os
+import requests
+import json
+
+API_URL = os.environ["API_URL"]
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+GIST_ID = os.environ["GIST_ID"]
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+
+GIST_URL = f"https://api.github.com/gists/{GIST_ID}"
+HEADERS = {
+    "Authorization": f"token {GIT_HUB_TOKEN}",
+    "Accept": "application/vnd.github+json"
+}
+
+def get_plafond():
+    r = requests.get(API_URL, timeout=10)
+    r.raise_for_status()
+    return r.json()["residuoPlafond"]  # verifica il campo
+
+def get_old_value():
+    r = requests.get(GIST_URL, headers=HEADERS)
+    r.raise_for_status()
+    content = r.json()["files"]["plafond.json"]["content"]
+    return int(json.loads(content)["value"])
+
+def save_value(value):
+    payload = {
+        "files": {
+            "plafond.json": {
+                "content": json.dumps({"value": value})
+            }
+        }
+    }
+    r = requests.patch(GIST_URL, headers=HEADERS, json=payload)
+    r.raise_for_status()
+
+def send_telegram(msg):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": msg
+    })
+
+def main():
+    current = get_plafond()
+    old = get_old_value()
+
+    if current != old:
+        send_telegram(
+            f"🚨 PLAFOND MODIFICATO\n"
+            f"Prima: {old}\n"
+            f"Ora: {current}"
+        )
+        save_value(current)
+
+if __name__ == "__main__":
+    main()
