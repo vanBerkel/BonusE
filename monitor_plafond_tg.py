0668516c007cec5f742888458585e155a5245907
vanBerkel <annispmt92@hotmail.it>
2025-12-17 08:41:51 +0100

    Update monitor_plafond_tg.py

--- monitor_plafond_tg.py
+++ monitor_plafond_tg.py
@@ -35,7 +35,7 @@ def get_old_value():
     return int(json.loads(content)["value"])
 
 def save_value(value):
-    print("DEBUG: HEADERS =", HEADERS_W[:20])
+    print("DEBUG: HEADERS =", HEADERS_W)
     payload = {
         "files": {
             "plafond.json": {
